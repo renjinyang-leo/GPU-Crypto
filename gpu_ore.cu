e59a4ec36dc292f8f1a5b0dbc94d236d172fa86a
renjinyang-leo <1980910250@qq.com>
2024-01-01 09:59:12 -0500

    reduce data copying and improve gpu computing parallelism

--- gpu_ore.cu
+++ gpu_ore.cu
@@ -8,6 +8,8 @@
 #include <cstring>
 #include <iomanip>
 #include <cuda.h>
+#include <assert.h>
+#include <cuda_runtime.h>
 
 #include "aes_key.h"
 
@@ -22,41 +24,7 @@ typedef unsigned long u32;
 #define PLAINTEXT_SIZE sizeof(uint64_t)
 #define MASK_SIZE 2
 
-// S盒，device，加密用
-__constant__ static const uint8_t sbox[256] = {
-    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5,
-    0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0,
-    0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc,
-    0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a,
-    0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
-    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0,
-    0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
-    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b,
-    0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
-    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85,
-    0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
-    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5,
-    0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
-    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17,
-    0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
-    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88,
-    0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
-    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c,
-    0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
-    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9,
-    0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
-    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6,
-    0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e,
-    0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
-    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94,
-    0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68,
-    0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
-};
+#define CHECK(x) if (x != cudaSuccess) {printf("Error: %s\n", cudaGetErrorString(x));printf("Exiting...\n");exit(EXIT_FAILURE);}
 
 //以下为T表
 u32 Te0[256] = {
@@ -492,7 +460,7 @@ __device__ void aes256_encrypt_ecb(uint8_t *buf_segment) {
     buf_segment[15]=s3&0xff;
 }
 
-__global__ void general_order_revealing_encryption_gpu(uint8_t *buf_device_plaintext, unsigned int *buf_device_ciphertext) {
+__global__ void general_order_revealing_encryption_gpu(uint64_t *buf_device_plaintext, unsigned long index, unsigned int *buf_device_ciphertext) {
     /**
      * 每一次截取明文的前bits位处理，并根据ECB规则将其补齐为16字节，补位的位填充0
      */
@@ -503,19 +471,12 @@ __global__ void general_order_revealing_encryption_gpu(uint8_t *buf_device_plain
     uint8_t target_bit = 0;
 
     uint8_t buf_segment[SEGMENT_SIZE] = {0};
-    unsigned int index = 1;
-    while (index <= SEGMENT_SIZE) {
-        if (index * 8 <= bits) {
-            buf_segment[index-1] = buf_device_plaintext[index-1];
-        } else {
-            buf_segment[index-1] = 0;
-        }
-        index++;
-    }
+    uint8_t *plaintext = (uint8_t *)&buf_device_plaintext[index];
+    memcpy(buf_segment, plaintext, bits/8);
     if (bits % 8 != 0) {
         unsigned int last_index = bits / 8;
         uint8_t additional_bits = bits % 8;
-        buf_segment[last_index] = buf_device_plaintext[last_index] & (uint8_t)(1<<additional_bits-1);
+        buf_segment[last_index] = plaintext[last_index] & (uint8_t)(1<<additional_bits-1);
         if (!((uint8_t)(buf_segment[last_index] & (uint8_t)(1<<(additional_bits-1))) == (uint8_t)0)) {
             target_bit = 1;
         }
@@ -527,6 +488,7 @@ __global__ void general_order_revealing_encryption_gpu(uint8_t *buf_device_plain
     }
 
     aes256_encrypt_ecb(buf_segment);
+
     /**
      * 对处理完的buf_segment取最后MASK_SIZE位，并写回记录结果的buf中
      * **/
@@ -534,51 +496,60 @@ __global__ void general_order_revealing_encryption_gpu(uint8_t *buf_device_plain
     mask = (mask + target_bit) % 4;
     unsigned long cipher_index = (threadIdx.x * 2) / 32;
     unsigned long cipher_bit = (threadIdx.x * 2) % 32;
-    printf("cipher_index = %ld, cipher_bit = %ld, mask = %d, target_bit = %d\n", cipher_index, cipher_bit, mask, target_bit);
-    atomicAdd(&buf_device_ciphertext[cipher_index], mask<<cipher_bit);
+    atomicAdd(&buf_device_ciphertext[index * 4 + cipher_index], ((unsigned int)mask)<<cipher_bit);
 }
 
-uint8_t *buf_device_plaintext;
-unsigned int *buf_device_ciphertext;
-
-uint8_t *buf_host_plaintext;
-unsigned int *buf_host_ciphertext;
-
-void encyrpt(uint64_t plaintext) {
-    memcpy(buf_host_plaintext, &plaintext, PLAINTEXT_SIZE);
-
-    cudaMemcpy(buf_device_plaintext, buf_host_plaintext, PLAINTEXT_SIZE, cudaMemcpyHostToDevice);
-    cudaMemset((void**)&buf_device_ciphertext, 0, PLAINTEXT_SIZE * MASK_SIZE);
-
-    general_order_revealing_encryption_gpu<<<1, 64>>>(buf_device_plaintext, buf_device_ciphertext);
-
-    cudaMemcpy(buf_host_ciphertext, buf_device_ciphertext, PLAINTEXT_SIZE * MASK_SIZE, cudaMemcpyDeviceToHost);
-    for (int i = 0; i < 4; i++) {
-       printf("%08X\n", buf_host_ciphertext[i]);
+__global__ void encyrpt(uint64_t *buf_device_plaintext, unsigned int *buf_device_ciphertext, unsigned int len, unsigned int prev_block) {
+    unsigned int index = (prev_block + blockIdx.x) * THREADS_PER_BLOCK + threadIdx.x;
+    if (index <= len - 1) {
+        general_order_revealing_encryption_gpu<<<1, 64>>>(buf_device_plaintext, index, buf_device_ciphertext);
     }
-    printf("\n");
 }
 
-void init_env() {
+uint64_t *buf_device_plaintext = nullptr;
+unsigned int *buf_device_ciphertext = nullptr;
+
+uint64_t *buf_host_plaintext = nullptr;
+unsigned int *buf_host_ciphertext = nullptr;
+
+void load_data(unsigned int len) {
+    CHECK(cudaMemcpy(buf_device_plaintext, buf_host_plaintext, PLAINTEXT_SIZE * len, cudaMemcpyHostToDevice));
+}
+
+void load_result(unsigned int len) {
+    unsigned int *buf_ciphertext = (unsigned int *)malloc(PLAINTEXT_SIZE * MASK_SIZE * len);
+    unsigned int copy_len = 4 * len;
+    for (int i = 0; i < copy_len; i++) {
+        buf_ciphertext[i] = buf_host_ciphertext[i];
+        //printf("%08X\n", buf_ciphertext[i]);
+    }
+}
+
+void init_env(const unsigned len) {
     // 设置gpu
     int deviceCount = 0;
-    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
-    if (error_id != cudaSuccess){
-        printf("Error: %s\n", cudaGetErrorString(error_id));
-        printf("Exiting...\n");
-        exit(EXIT_FAILURE);
-    }
+    CHECK(cudaGetDeviceCount(&deviceCount));
     if (deviceCount == 0){
         printf("There are no available device(s) that support CUDA\n");
+        CHECK(cudaDeviceReset());
         exit(EXIT_FAILURE);
     }
 
-    cudaMemcpyToSymbol(cuda_Te0,Te0,256*sizeof(u32));
-    cudaMemcpyToSymbol(cuda_Te1,Te1,256*sizeof(u32));
-    cudaMemcpyToSymbol(cuda_Te2,Te2,256*sizeof(u32));
-    cudaMemcpyToSymbol(cuda_Te3,Te3,256*sizeof(u32));
+    int dev = 0;
+	CHECK(cudaSetDevice(dev));
+	cudaDeviceProp deviceProp;
+	CHECK(cudaGetDeviceProperties(&deviceProp, dev));
+	if (!deviceProp.canMapHostMemory) {
+		printf("Device %d(%s) does not support mapping CPU host memory!\n", dev, deviceProp.name);
+        CHECK(cudaDeviceReset());
+		exit(EXIT_SUCCESS);
+	}
+    CHECK(cudaSetDeviceFlags(cudaDeviceMapHost));
 
-    cudaMemcpyToSymbol(sbox, sbox, sizeof(uint8_t)*256);
+    CHECK(cudaMemcpyToSymbol(cuda_Te0,Te0,256*sizeof(u32)));
+    CHECK(cudaMemcpyToSymbol(cuda_Te1,Te1,256*sizeof(u32)));
+    CHECK(cudaMemcpyToSymbol(cuda_Te2,Te2,256*sizeof(u32)));
+    CHECK(cudaMemcpyToSymbol(cuda_Te3,Te3,256*sizeof(u32)));
 
     // 生成密钥
     uint8_t seed[32];
@@ -590,17 +561,52 @@ void init_env() {
     for (int i = 0; i < 240; i += 4) {
         keywords_host[i/4]=Byte2Word_host(aes_key.key[i],aes_key.key[i+1],aes_key.key[i+2],aes_key.key[i+3]);
     }
-    cudaMemcpyToSymbol(keywords, keywords_host, sizeof(u32)*60);
+    free(aes_key.key);
 
-    cudaMalloc((void**)&buf_device_plaintext, PLAINTEXT_SIZE);
-    cudaMalloc((void**)&buf_device_ciphertext, PLAINTEXT_SIZE * MASK_SIZE);
+    CHECK(cudaMemcpyToSymbol(keywords, keywords_host, sizeof(u32)*60));
 
-    cudaMallocHost((void**)&buf_host_ciphertext, PLAINTEXT_SIZE * MASK_SIZE, cudaHostAllocDefault);
-    cudaMallocHost((void**)&buf_host_plaintext, PLAINTEXT_SIZE, cudaHostAllocDefault);
+    CHECK(cudaMalloc((void**)&buf_device_plaintext, PLAINTEXT_SIZE * len));
+
+    CHECK(cudaHostAlloc((void**)&buf_host_plaintext, PLAINTEXT_SIZE * len, cudaHostAllocWriteCombined));
+    CHECK(cudaHostAlloc((void**)&buf_host_ciphertext, PLAINTEXT_SIZE * MASK_SIZE * len, cudaHostAllocMapped));
+    CHECK(cudaMemset(buf_host_ciphertext, 0, PLAINTEXT_SIZE * MASK_SIZE * len));
+
+    CHECK(cudaHostGetDevicePointer((void**)&buf_device_ciphertext, buf_host_ciphertext, 0));
+}
+
+void free_env() {
+    CHECK(cudaFree(buf_device_plaintext));
+
+    CHECK(cudaFreeHost(buf_host_plaintext));
+    CHECK(cudaFreeHost(buf_host_ciphertext));
+
+    CHECK(cudaDeviceReset());
 }
 
 int main() {
-    init_env();
-    encyrpt(100000);
-    encyrpt(2021232);
+    const unsigned N = 1000000;
+
+    clock_t start,end;
+    start=clock();
+
+    init_env(N);
+    for (uint64_t i = 0; i < N; i++) {
+        buf_host_plaintext[i] = N-i;
+    }
+
+    load_data(N);
+    double batch = 4;
+    dim3 dimBlock(batch);
+    dim3 dimGrid(THREADS_PER_BLOCK);
+    for (unsigned int blocks = 0; blocks * THREADS_PER_BLOCK < N; blocks += (unsigned int)batch) {
+        encyrpt<<<dimBlock, dimGrid>>>(buf_device_plaintext, buf_device_ciphertext, N, blocks);
+    }
+    load_result(N);
+
+    end=clock();
+	double cost=(double)(end-start)*1000/CLOCKS_PER_SEC;
+    printf("cost time = %lf ms \n", cost);
+
+    free_env();
+    return 0;
 }
